3873c1639c94ccfeb8553ba6094a8bfd94f45c0b
Judith van Leersum <judithvleersum@gmail.com>
2022-08-25 17:36:58 +0200

    Add marker by pressing on the map

--- screens/Map.js
+++ screens/Map.js
@@ -1,21 +1,44 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps"
+import MapView, { Marker } from "react-native-maps";
 
 const Map = () => {
-  const region = {
-    latitude: 37.78, 
-    longitude: -122.43, 
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-  }
+  const [selectedLocation, setSelectedLocation] = useState();
 
-  return <MapView initialRegion={region} style={styles.map}></MapView>
-}
+  const region = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lon = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ latitude: lat, longitude: lon });
+  };
+
+  return (
+    <MapView
+      initialRegion={region}
+      style={styles.map}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && <Marker
+        title="Picked Location"
+        coordinate={{
+          latitude: selectedLocation.latitude,
+          longitude: selectedLocation.longitude,
+        }}
+      />}
+    </MapView>
+  );
+};
 
 export default Map;
 
 const styles = StyleSheet.create({
   map: {
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
