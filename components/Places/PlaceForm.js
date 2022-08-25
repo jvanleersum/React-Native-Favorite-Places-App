6e1ac5535e00081531b620ea74120affbfe242e1
Judith van Leersum <judithvleersum@gmail.com>
2022-08-25 12:24:14 +0200

    Form file created and added to AddPlace screen

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text } from "react-native"
+
+const PlaceForm = () => {
+  return <View>
+    <Text>The Place Form</Text>
+  </View>
+}
+
+export default PlaceForm
+
+const styles = StyleSheet.create({})
