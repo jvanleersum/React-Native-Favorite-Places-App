b519697c9392dccf247cd34b618e9d8a13a2d017
Judith van Leersum <judithvleersum@gmail.com>
2022-08-25 12:07:51 +0200

    Added basic PlacesList without styling

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,8 +1,35 @@
-import { FlatList } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 import PlacesListItem from "./PlacesListItem";
 
-const PlacesList = ({places}) => {
-  return <FlatList data={places} renderItem={({item}) => <PlacesListItem place={item} />} />
-}
+const PlacesList = ({ places }) => {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+
+      <Text style={styles.fallbackText}>
+        No places added yet - start adding some!
+      </Text>
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={places}
+      renderItem={({ item }) => <PlacesListItem place={item} />}
+    />
+  );
+};
 
 export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16
+  }
+});
