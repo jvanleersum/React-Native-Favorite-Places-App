bf0d78a433a8d0a985cc6d1d5aed26650e441125
Judith van Leersum <judithvleersum@gmail.com>
2022-08-25 12:03:30 +0200

    PlacesListItem coded without styling

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,7 +1,8 @@
 import { FlatList } from "react-native";
+import PlacesListItem from "./PlacesListItem";
 
 const PlacesList = ({places}) => {
-  return <FlatList data={places} renderItem={(itemData) => {}}/>
+  return <FlatList data={places} renderItem={({item}) => <PlacesListItem place={item} />} />
 }
 
 export default PlacesList;
--- components/Places/PlacesListItem.js
+++ components/Places/PlacesListItem.js
@@ -1,5 +1,17 @@
-const PlacesListItem = () => {
+import { Image, Pressable, Text, View, StyleSheet } from "react-native";
 
+const PlacesListItem = ({place, onPress}) => {
+  return <Pressable onPress={onPress}>
+    <Image source={{uri: place.imageUri}}/>
+    <View>
+      <Text>{place.title}</Text>
+      <Text>{place.address}</Text>
+    </View>
+  </Pressable>
 }
 
 export default PlacesListItem;
+
+const styles = StyleSheet.create({
+  
+})
