eef3c1f999e5c775ed98b4fe4aa141edba513a22
Judith van Leersum <judithvleersum@gmail.com>
2022-08-25 16:07:51 +0200

    Added placeholder for mapPreview

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,14 +1,20 @@
 import { View, StyleSheet } from "react-native";
+
 import OutlinedButton from "../ui/OutlinedButton";
+import { Colors } from "../../constants/colors";
 
 const LocationPicker = () => {
+  const currentLocationHandler = () => {}
+
+  const selectOnMapHandler = () => {}
+
   return <View>
-    <View>
+    <View style={styles.mapPreview}>
     
     </View>
     <View style={styles.buttonsContainer}>
-      <OutlinedButton icon="location">Current location</OutlinedButton>
-      <OutlinedButton icon="map">Select on map</OutlinedButton>
+      <OutlinedButton icon="location" onPress={currentLocationHandler}>Current location</OutlinedButton>
+      <OutlinedButton icon="map" onPress={selectOnMapHandler}>Select on map</OutlinedButton>
     </View>
   </View>
 }
@@ -16,6 +22,15 @@ const LocationPicker = () => {
 export default LocationPicker;
 
 const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 6
+  },
   buttonsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
