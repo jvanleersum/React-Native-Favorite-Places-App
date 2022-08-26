b8095062669b5090bf7fdefa8ffc385cdf70ccb6
Judith van Leersum <judithvleersum@gmail.com>
2022-08-26 11:23:11 +0200

    Updated accuracy

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -50,7 +50,7 @@ const LocationPicker = ({onPickLocation}) => {
     if (!hasPermission) {
       return;
     }
-    const location = await getCurrentPositionAsync({accuracy: LocationAccuracy.High});
+    const location = await getCurrentPositionAsync({accuracy: LocationAccuracy.Low});
     const coordinates = {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
